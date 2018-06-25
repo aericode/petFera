735ef87a63cc7bb3857a9d8217f53c24f91d42c1
Eric Andrade <vloxflox@gmail.com>
2018-06-24 23:53:52 -0300

    primeiro teste. setBatismo causa falha de segmentação

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 #include <string>
 
 #include "../include/funcionario.h"
+#include "../include/veterinario.h"
+#include "../include/tratador.h"
 #include "../include/animal.h"
 
 int main(){
+
+	Animal ferinha;
+
+	ferinha.setNome("Gatimon");
+	std::cout<<ferinha.getNome()<<std::endl;
 
     return 0;
 }
