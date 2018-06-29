6ad8ca40d37e50de7255209bc1fb7a8c7f80de99
Eric Andrade <vloxflox@gmail.com>
2018-06-29 17:44:39 -0300

    transferindo busca de dados para a classe loja

---
+++ src/loja.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+
+#include "../include/funcionario.h"
+#include "../include/veterinario.h"
+#include "../include/tratador.h"
+#include "../include/animal.h"
+#include "../include/reptil.h"
+#include "../include/anfibio.h"
+#include "../include/mamifero.h"
+#include "../include/animalSilvestre.h"
+#include "../include/exotico.h"
+#include "../include/nativo.h"
+#include "../include/aveExotica.h"
+#include "../include/aveNativa.h"
+#include "../include/loja.h"
+
+
+void Loja::carregarFuncionarios(){
+	std::string tipo_funcionario;
+	std::string::size_type sz; // alias do size_t de string
+
+	std::ifstream ip("./data/funcionario_db.csv");
+	
+	if(!ip.is_open()){
+		std::cout << "ERRO: arquivo funcionario_db.csv nao foi aberto"<<std::endl;
+	}
+
+	int func_id;
+	std::string func_nome;
+	std::string func_cpf;
+	short int func_idade;
+	std::string func_tipo_sanguineo;
+	char func_fatorRH;
+	std::string func_especialidade;
+	
+	while(ip.good()){
+		//ID
+		getline(ip,temp,';');
+		func_id = std::stoi(temp,&sz);//converte a string temp em int
+
+		//TIPO PARA INICIALIZAÇÃO
+		getline(ip,tipo_funcionario,';');
+
+		//NOME
+		getline(ip,func_nome,';');
+
+		//CPF
+		getline(ip,func_cpf,';');
+
+		//IDADE
+		getline(ip,temp,';');
+		func_idade = (short)std::stoi(temp,&sz);//converte a string temp em int
+
+		//TIPO SANGUINEO
+		getline(ip,func_tipo_sanguineo,';');
+
+		//FATOR RH
+		getline(ip,temp,';');
+		func_fatorRH = *temp.c_str();//converte a string temp em char
+
+		//ESPECIALIDADE
+		getline(ip,func_especialidade,'\n');
+
+
+
+	}
+}
